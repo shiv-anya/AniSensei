c6c247d9464051868209cf9e667b09af55d0d624
shiv-anya <sg90883@gmail.com>
2025-08-16 16:17:18 +0530

    added genres page

---
+++ src/app/genres/page.tsx
@@ -0,0 +1,55 @@
+const AniListGenres = [
+  "Action",
+  "Adventure",
+  "Comedy",
+  "Drama",
+  "Slice of Life",
+  "Fantasy",
+  "Magic",
+  "Supernatural",
+  "Mystery",
+  "Psychological",
+  "Romance",
+  "Sci-Fi",
+  "Sports",
+  "Ecchi",
+  "Harem",
+  "Hentai",
+  "Isekai",
+  "Historical",
+  "Military",
+  "Music",
+  "Mecha",
+  "Vampire",
+  "Horror",
+  "Thriller",
+  "Shounen",
+  "Shoujo",
+  "Josei",
+  "Seinen",
+  "Sport",
+  "Super Power",
+  "Magical Girl",
+  "Martial Arts",
+  "Demons",
+  "School",
+  "Game",
+  "Police",
+  "Space",
+  "Drama",
+  "Yuri",
+  "Yaoi",
+  "Fantasy",
+  "Ecchi",
+  "Gender Bender",
+];
+
+export default function Genres() {
+  return (
+    <section className="h-auto w-[80%] mx-auto py-40">
+      <div>
+        <h2>Genres</h2>
+      </div>
+    </section>
+  );
+}
