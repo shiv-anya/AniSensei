1e9f618531f97bdcc13faed36179ab10d356c5bc
shiv-anya <sg90883@gmail.com>
2025-08-16 16:18:03 +0530

    added you may like section in all separate anime pages

---
+++ src/app/_components/GenreRecommendations.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useState, useEffect } from "react";
+import { RiStarSmileLine } from "react-icons/ri";
+import ListCarousel from "./ListCarousel";
+import { GENRE_RECOMMENDATIONS } from "../_graphql/queries";
+import { fetchAniList } from "../_lib/fetchAniList";
+
+export default function GenreRecommendations({ genres }) {
+  const [recommendedAnimes, setRecommendedAnimes] = useState([]);
+  const loadAnimes = async () => {
+    const data = await fetchAniList({
+      query: GENRE_RECOMMENDATIONS,
+      variables: { genres },
+    });
+
+    setRecommendedAnimes(data.Page.media);
+  };
+  useEffect(() => {
+    loadAnimes();
+  }, []);
+  return (
+    <section className="pt-16">
+      <ListCarousel
+        animes={recommendedAnimes}
+        title={"you may like"}
+        icon={<RiStarSmileLine />}
+        height={"40"}
+        itemsCountPerPage={7}
+      />
+    </section>
+  );
+}
