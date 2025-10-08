edd8a487bc2517d8d193fd280bfa64a73473a66a
shiv-anya <sg90883@gmail.com>
2025-10-08 20:19:09 +0530

    added sensei page and loader for browser/[id] page

---
+++ src/app/(with-nav)/sensei/page.tsx
@@ -0,0 +1,9 @@
+export default function Sensei() {
+  return (
+    <section className="h-screen mx-auto w-[80%] flex justify-center items-center">
+      <div className="text-4xl text-center">
+        AI recommendation system, yet to implement. :{")"}
+      </div>
+    </section>
+  );
+}
